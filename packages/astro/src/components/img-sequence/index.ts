e37fdf32f757c143fdb54c089d076d51cc82f176
Pavel Mazhuga <pavelmazhuga@yandex.ru>
2025-11-19 14:53:18 +0700

    feat(astro): add Astro package with ImgSequence component and build configuration

---
+++ packages/astro/src/components/img-sequence/index.ts
@@ -0,0 +1,3 @@
+export { default as ImgSequence } from './ImgSequence.astro';
+export type { Props as ImgSequenceProps } from './ImgSequence.astro';
+
---
+++ packages/astro/vite.config.ts
@@ -0,0 +1,20 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+    build: {
+        lib: {
+            name: 'test',
+            entry: resolve(__dirname, './src/index.ts'),
+            fileName: (format, name) => {
+                return `${name}.${format}.js`;
+            },
+        },
+    },
+    plugins: [dts({ tsconfigPath: './tsconfig.json', exclude: ['**/*.test.ts'] })],
+});
+
